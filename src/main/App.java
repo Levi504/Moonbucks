fc341fbba868f4d139c5287781d407d0b14010ec
Levi504 <levi.hoimian2@gmail.com>
2026-09-15 15:24:02 +0200

    Building Order class

--- src/main/App.java
+++ src/main/App.java
@@ -2,14 +2,21 @@ package main;
 
 import model.OrderItem;
 import model.Product;
+import model.Customer;
+import model.Order;
 
 public class Tests{
    public static void main(String[] args) {
       Product p1 = new Product("P001", "Capuccino", 9.99);
-      OrderItem ot1 = new OrderItem(p1, "Or.001", 3);
-      System.out.println("Votre Commande: " +
-                        ot1.getProduct().getProductName() + " " + 
-                        ot1.getOrderID() + " " + 
-                        ot1.getQuantity());
+      OrderItem coffee = new OrderItem(p1, "Or.001", 3);
+      Customer levi = new Customer("C001",
+                                 "Levi DUBOIS",
+                                 "5 Av. Jules Vernes 75600",
+                                 "+33 6 05 04 02 03");
+      Order o1 = new Order("O001", levi);
+      System.out.println("Id de commande: " +
+                        o1.getOrderID() + "\n" +
+                        "Id du client: " +
+                        o1.getCustomer().getCustomerID());
    }
 }
--- src/model/Order.java
+++ src/model/Order.java
@@ -1,1 +1,61 @@
 package model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Order {
+    //Fields
+    private String orderID;
+    private Customer customer;
+    private List<OrderItem> orderItems;
+
+    //Constructor
+    public Order(String orderID, Customer customer) {
+        setOrderID(orderID);
+        setCustomer(customer);
+        this.orderItems = new ArrayList<>();
+    }
+
+    //Getters
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    //Setters
+    public void setOrderID(String newOrderID) {
+        if (newOrderID == null || newOrderID.trim().isEmpty()) {
+            throw new IllegalArgumentException("OrderID cannot be empty");
+        }
+        this.orderID = newOrderID;
+    }
+
+    public void setCustomer(Customer newCustomer) {
+        if (newCustomer == null) {
+            throw new IllegalArgumentException("Customer is missing");
+        }
+        this.customer = newCustomer;
+    }
+
+    //Methods
+    public void addOrderItem(OrderItem newOrderItem) {
+        if (newOrderItem == null) {
+            throw new IllegalArgumentException("OrderItem cannot be empty");
+        }
+        this.orderItems.add(newOrderItem);
+    }
+
+    public void removeOrderItem(OrderItem existingOrderItem) {
+        if (existingOrderItem == null || !orderItems.contains(existingOrderItem)) {
+            throw new IllegalArgumentException("OrderItem is empty or not in this order");
+        }
+        this.orderItems.remove(existingOrderItem);
+    }
+}
